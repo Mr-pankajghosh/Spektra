d0abc6b04c64ef2825688a87762adf11aeb6409a
Pankaj Ghosh <ghoshpankaj260@gmail.com>
2025-10-28 19:32:07 +0530

    Fix axios baseURL and rebuild frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -11,7 +11,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : `${window.location.origin}/api`;
+    : `${window.location.origin}`;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
