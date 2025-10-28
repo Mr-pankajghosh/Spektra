07dddff0ede41cec81886da3e644db16ea58cba6
Pankaj Ghosh <ghoshpankaj260@gmail.com>
2025-10-28 20:17:46 +0530

    Fix axios baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,18 @@
+// import axios from 'axios';
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+// export const axiosInstance = axios.create({
+//     baseURL: BASE_URL,
+//     withCredentials: true,//send cookies with requests
+// });
+
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,//send cookies with requests
-});
+const BASE_URL = import.meta.env.DEV
+  ? "http://localhost:5001/api"
+  : `${import.meta.env.VITE_API_URL}/api`;
 
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
